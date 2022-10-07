23d074bb90300da2a7cf36b0f632abd8f3681a43
Robert Shepley <shepleysound@gmail.com>
2022-10-07 12:13:07 -0700

    Modularize game state with a class and class instance.

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ require('dotenv').config()
 const PORT = process.env.PORT;
 const io = require('socket.io')(PORT || 3002);
 
-let currentWord = 'wowee';
+const wordsArray = require('./lib/words.json');
 
+const GameState = require('./lib/game-state');
+
+// Initialize the game state with the proper IO server and words to choose from.
+const gs = new GameState(io, wordsArray);
+
+console.log(gs.currentWord)
 io.on('connection', (socket) => {
-  console.log('hello', PORT, socket.id)
+  console.log('hello', PORT, socket.id);
   console.log(io.of('/').server.engine.clientsCount);
 
+  // Whenever a player disconnects
   socket.on('disconnect', () => {
     console.log('A client disconnected.');
   });
 
+  // Whenever a player joins the game
   socket.on('join-game', (player) => {
     console.log(`${player} joined the game!`);
     socket.join('game-room');
@@ -21,11 +29,16 @@ io.on('connection', (socket) => {
     socket.emit('game-start');
   });
 
+  // Whenever a player sends a word attempt during the game session
   socket.on('attempt', (input, player) => {
-    console.log('attempt made - ', input);
-    if (input === currentWord) {
+    console.log('attempt made - ', input, player);
+    console.log(gs.currentWord)
+    if (input === gs.currentWord) {
       socket.emit('success');
       socket.to('game-room').emit('broadcast-success', player);
+      gs.newWord();
+      clearInterval(gs.wordTimer);
+      gs.wordTimer = setInterval(gs.newWord, 5000);
     }
   });
 

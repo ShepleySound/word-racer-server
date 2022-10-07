291bdf389bc3c4373dc3ac15b41115acc83be756
Robert Shepley <shepleysound@gmail.com>
2022-10-06 17:54:00 -0700

    Add events for client broadcasting.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config()
 const PORT = process.env.PORT;
-const io = require('socket.io')(PORT || 3002;
+const io = require('socket.io')(PORT || 3002);
 
 
 io.on('connection', (socket) => {
@@ -10,6 +10,13 @@ io.on('connection', (socket) => {
   console.log(io.of('/').server.engine.clientsCount);
 
   socket.on('disconnect', () => {
-    console.log('A client disconnected.')
-  })
-})
+    console.log('A client disconnected.');
+  });
+
+  socket.on('join-game', (name) => {
+    console.log(`${name} joined the game!`);
+    socket.join('game-room');
+    io.to('game-room').emit('joined-event', name)
+  });
+
+});

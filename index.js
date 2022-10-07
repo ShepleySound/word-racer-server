ba54092bde6766fbdee056bdd030fd097efee6cd
Robert Shepley <shepleysound@gmail.com>
2022-10-07 10:26:40 -0700

    Send message for game state and word matches.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const PORT = process.env.PORT;
 const io = require('socket.io')(PORT || 3002);
 
+let currentWord = 'wowee';
 
 io.on('connection', (socket) => {
   console.log('hello', PORT, socket.id)
@@ -13,10 +14,19 @@ io.on('connection', (socket) => {
     console.log('A client disconnected.');
   });
 
-  socket.on('join-game', (name) => {
-    console.log(`${name} joined the game!`);
+  socket.on('join-game', (player) => {
+    console.log(`${player} joined the game!`);
     socket.join('game-room');
-    socket.to('game-room').emit('joined-event', name)
+    socket.to('game-room').emit('broadcast-joined-event', player);
+    socket.emit('game-start');
+  });
+
+  socket.on('attempt', (input, player) => {
+    console.log('attempt made - ', input);
+    if (input === currentWord) {
+      socket.emit('success');
+      socket.to('game-room').emit('broadcast-success', player);
+    }
   });
 
 });
